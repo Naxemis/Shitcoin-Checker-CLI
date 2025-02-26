1feb3b65b6b3c4a2faf4dacca1b226eab2659ec0
Szymon Lenard <szymon.lenard@icloud.com>
2025-02-26 17:20:26 +0100

    Added ground for all needed functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 #!/usr/bin/node
+
+const readline = require('readline');
+const axios = require('axios');
+const clc = require('cli-color');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function checkDexScreener(tokenAddress) {
+    
+}
+
+async function checkRugCheck(tokenAddress) {
+
+}
+
+async function checkBubblemaps(tokenAddress) {
+
+}
+
+function errorMessage(message) {
+    console.log(clc.red('ERROR: ' + message));
+}
+
+function startPorgram() {
+    rl.question('Enter token address: ', (tokenAddress) => {
+        checkDexScreener(tokenAddress);
+        checkRugCheck(tokenAddress);
+        checkBubblemaps(tokenAddress);
+        rl.close();
+    })
+}
+
+startPorgram();
